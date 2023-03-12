473bdaede1c853b1894fe1bbf6d84180df55eedc
wpao <wpaozan@gmail.com>
2023-03-12 14:35:57 +0800

    UI/UX mern-blog V1

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -6,13 +6,14 @@ import { useHistory } from 'react-router-dom'
 
 const BlogItem = (props) => {
     const history = useHistory()
+    const { image, title, name, date, body } = props
     return (
         <div className='blog-item'>
-            <img className='image-thumb' src={LoginBg} alt='post' />
+            <img className='image-thumb' src={image} alt='post' />
             <div className='content-detail'>
-                <p className='title'>blog item</p>
-                <p className='author'>author</p>
-                <p className='body'>lorem</p>
+                <p className='title'>{title}</p>
+                <p className='author'>{name} - {date}</p>
+                <p className='body'>{body}</p>
                 <Button title='view detail' onClick={() => history.push('/detail-blog')} />
             </div>
         </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BlogItem, Button, Gap } from '../../components'
 import './home.scss'
 import { useHistory } from 'react-router-dom';
+import Axios from 'axios';
 
 const Home = (props) => {
+  const [dataBlog, setDataBlog] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://localhost:4000/v1/blog/posts?page=1&perPage=2')
+      // Axios.get('http://localhost:4000/v1/blog/posts')
+      .then(result => {
+        console.log('data API : ', result.data)
+        const responseAPI = result.data
+
+        setDataBlog(responseAPI.data)
+      })
+      .catch(err => console.log('error : ', err))
+  }, [])
+
   const history = useHistory();
   return (
     <div className='home-page-wrapper'>
@@ -12,10 +27,14 @@ const Home = (props) => {
       </div>
       <Gap height={20} />
       <div className='content-wrapper'>
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map(blog => <BlogItem
+          key={blog._id}
+          image={`http://localhost:4000/${blog.image}`}
+          title={blog.title}
+          body={blog.body}
+          name={blog.author.name}
+          date={blog.createdAt}
+        />)}
       </div>
       <div className='pagination'>
         <Button title="previous" />
